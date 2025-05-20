b58097eb42dfa7583969160d100a89ddabfb9dc6
cisasmendi <cisasmendi@unca.edu.ar>
2025-05-20 16:55:16 -0300

    fix: Update Docker run command to use environment variables for database and RabbitMQ configuration; add dotenv dependency and load environment variables in application

--- src/main/java/com/cisasmendi/crud_conceptos/CrudConceptosApplication.java
+++ src/main/java/com/cisasmendi/crud_conceptos/CrudConceptosApplication.java
@@ -5,14 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication(scanBasePackages = "com.cisasmendi.crud_conceptos")
-@EntityScan(basePackages = "com.cisasmendi.crud_conceptos.model") 
+@EntityScan(basePackages = "com.cisasmendi.crud_conceptos.model")
 @EnableRabbit
 public class CrudConceptosApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(CrudConceptosApplication.class, args);
 	}
 
